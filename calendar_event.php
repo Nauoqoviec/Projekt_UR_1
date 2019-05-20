cac0141af86adde50e369c29033eb66fce40b49c
Nauoqoviec <mat.nycz@onet.pl>
2019-05-20 21:50:04 +0200

    Lista - kalendarz wydarzeń

---
+++ calendar_event.php
@@ -0,0 +1,88 @@
+<?php include 'head.php' ?>
+
+<body>
+
+    <?php include 'header.php' ?>
+    
+    <div class="bg_event" style="background-image: url('img/nk.jpg')">
+        <h3 class="title_event">Nocny Kochanek</h3>
+    </div>
+    <div class="container events">
+        <div class="content">
+        <h4> Nadchodzące wydarzenia</h4>
+            <div class="event">
+                <table>
+                    <tr>
+                        <td class="day">22</td>
+                        <td class="title">KATOWICE, Polska</td>
+                    </tr>
+                    <tr>
+                        <td class="month">LIP</td>
+                        <td class="place">Katowice, Spodek - Sob. 21.30</td>
+                    </tr>
+                </table>
+                <button class="button">Zamów</button>
+            </div>
+            
+            <div class="event">
+                <table>
+                    <tr>
+                        <td class="day">22</td>
+                        <td class="title">KATOWICE, Polska</td>
+                    </tr>
+                    <tr>
+                        <td class="month">LIP</td>
+                        <td class="place">Katowice, Spodek - Sob. 21.30</td>
+                    </tr>
+                </table>
+                <button class="button">Zamów</button>
+            </div>
+            
+            <div class="event">
+                <table>
+                    <tr>
+                        <td class="day">22</td>
+                        <td class="title">KATOWICE, Polska</td>
+                    </tr>
+                    <tr>
+                        <td class="month">LIP</td>
+                        <td class="place">Katowice, Spodek - Sob. 21.30</td>
+                    </tr>
+                </table>
+                <button class="button">Zamów</button>
+            </div>
+            
+            <div class="event">
+                <table>
+                    <tr>
+                        <td class="day">22</td>
+                        <td class="title">KATOWICE, Polska</td>
+                    </tr>
+                    <tr>
+                        <td class="month">LIP</td>
+                        <td class="place">Katowice, Spodek - Sob. 21.30</td>
+                    </tr>
+                </table>
+                <button class="button">Zamów</button>
+            </div>
+            
+            <div class="event">
+                <table>
+                    <tr>
+                        <td class="day">22</td>
+                        <td class="title">KATOWICE, Polska</td>
+                    </tr>
+                    <tr>
+                        <td class="month">LIP</td>
+                        <td class="place">Katowice, Spodek - Sob. 21.30</td>
+                    </tr>
+                </table>
+                <button class="button">Zamów</button>
+            </div>
+        </div>
+        <aside class="sidebar">
+            
+        </aside>
+    </div>
+<hr style="width: 90%; clear: both; visibility: hidden;" />
+<?php include 'footer.php' ?>
--- head.php
+++ head.php
@@ -10,8 +10,11 @@
         
         <link rel="stylesheet" type="text/css" href="styles/normalize.css">
         <link rel="stylesheet" type="text/css" href="styles/style.css">
+        
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        
         <script type="text/javascript" src="js/onscroll.js"></script>
         <script type="text/javascript" src="js/onclick.js"></script>
     
